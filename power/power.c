854c04ce4a05591729aa51dbc641df13c259cf07
Joshua Choo <joshuachoo.jc@gmail.com>
2017-04-18 11:48:49 +0800

    Implement different interaction boost policies for touch and flings

--- power/power.c
+++ power/power.c
@@ -424,12 +424,19 @@ static void power_hint(struct power_module *module, power_hint_t hint,
                 return;
             }
 
-            int duration = 1500; // 1.5s by default
+            // Default boost duration for taps
+            int min_duration = 350; // 0.350s by default
+            int duration = min_duration;
+            bool isFling = false;
+
+            // Boost duration for scrolls/flings
+            // Minimum boost duration for flings is equal to the minimum duration for taps
             if (data) {
-                int input_duration = *((int*)data) + 750;
-                if (input_duration > duration) {
+                int input_duration = *((int*)data) + 200;
+                if (input_duration > min_duration) {
                     duration = (input_duration > 5750) ? 5750 : input_duration;
                 }
+				isFling = true;
             }
 
             struct timespec cur_boost_timespec;
@@ -446,13 +453,22 @@ static void power_hint(struct power_module *module, power_hint_t hint,
 
             // Scheduler is EAS.
             if (is_eas_governor(governor)) {
-                // Setting the value of foreground schedtune boost to 50 and
-                // scaling_min_freq to 1100MHz.
-                int eas_interaction_resources[] = { MIN_FREQ_BIG_CORE_0, 1100, 
-                                                    MIN_FREQ_LITTLE_CORE_0, 1100, 
-                                                    STOR_CLK_SCALE_DIS, 0x32, 
-                                                    CPUBW_HWMON_MIN_FREQ, 0x33};
-                interaction(duration, sizeof(eas_interaction_resources)/sizeof(eas_interaction_resources[0]), eas_interaction_resources);
+                // Scrolls/flings
+                if (isFling) {
+                    int eas_interaction_resources[] = { MIN_FREQ_BIG_CORE_0, 1113, 
+                                                        MIN_FREQ_LITTLE_CORE_0, 1113, 
+                                                        STOR_CLK_SCALE_DIS, 0x0A, // For changing top-app boost to 10
+                                                        CPUBW_HWMON_MIN_FREQ, 0x33};
+                    interaction(duration, sizeof(eas_interaction_resources)/sizeof(eas_interaction_resources[0]), eas_interaction_resources);
+                }
+                // Taps
+                else {
+                    int eas_interaction_resources[] = { MIN_FREQ_BIG_CORE_0, 729, 
+                                                        MIN_FREQ_LITTLE_CORE_0, 729, 
+                                                        //STOR_CLK_SCALE_DIS, 0x32, // For changing top-app boost to 50
+                                                        CPUBW_HWMON_MIN_FREQ, 0x33};
+                    interaction(duration, sizeof(eas_interaction_resources)/sizeof(eas_interaction_resources[0]), eas_interaction_resources);
+                }
             } else { // Scheduler is HMP.
                 int hmp_interaction_resources[] = { CPUBW_HWMON_MIN_FREQ, 0x33, 
                                                     MIN_FREQ_BIG_CORE_0, 1000, 
